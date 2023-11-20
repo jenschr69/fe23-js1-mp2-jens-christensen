de97ff77944cc264e3549e05cb944726c5b869d9
Jens Chr. Christensen <jcc@jenschrchristensen.com>
2023-11-20 22:27:53 +0100

    Adding visibility for game

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+// Når spelaren har skrivit in sitt namn och klickat på knappen "Lägg till spelare",
+// så skall input fältet för namn döljas och namn på spelarna, score och knappar visas - Opacity = 100%.
+
