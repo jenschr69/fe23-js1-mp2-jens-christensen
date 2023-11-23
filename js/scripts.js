f187a72de5ffe748fe40247ce82ebcdaa9e86d83
Jens Chr. Christensen <jcc@jenschrchristensen.com>
2023-11-23 14:16:19 +0100

    Updating kodplanering

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 // Når spelaren har skrivit in sitt namn och klickat på knappen "Lägg till spelare",
 // så skall input fältet för namn döljas och namn på spelarna, score och knappar visas - Opacity = 100%.
 
+let computerPlayerChoiceNum = Math.floor( Math.random()*3 ) ;
+let computerPlayerChoice = '';
+
+if(computerPlayerChoiceNum == 1) {
+    computerPlayerChoice = 'rock';
+}
+else if (computerPlayerChoiceNum == 2){
+    computerPlayerChoice = 'paper';
+}
+else if (computerPlayerChoiceNum == 3){
+    computerPlayerChoice = 'sissor';
+}
+
+console.log (computerPlayerChoice);
+
