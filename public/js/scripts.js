630edeb949395cb6bfa81d9a8fb2656f7f69d26c
Jens Christensen <jcc@jenschr.net>
2024-03-27 11:13:11 +0100

    Adding instructions link about eventlistener

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,11 @@
 // Receive human players name
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_form_submit
+
+// Register click on form button with eventlistener
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_addeventlistener_displaydate
+
+// When received, hide input form
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_visibility
 
 // Register human players choice
 
