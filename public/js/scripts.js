8d5c9e16dca8849143bb85f7c1c0a30fa32d1ae2
Jens Christensen - Glodexa <jens@glodexa.com>
2024-05-26 14:40:45 +0200

    Adding files

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,27 @@
 // Receive human players name
-let humanPlayerName = document.getElementById('humanplayername');
-console.log('humanPlayerName');
+let loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let humanplayername = document.getElementById("humanplayername");
+
+    if (username.value == "") {
+        alert("Ensure you input a value in both fields!");
+    } else {
+        // perform operation with form input
+        alert("This form has been successfully submitted!");
+        console.log(
+            `This form has a username of ${humanplayername.value}`
+        );
+
+        humanplayername.value = "";
+
+        document.getElementById("humanPlayerName").innerHTML = humanplayername.value;
+    }
+});
+
+document.getElementById("humanPlayerName").innerHTML = humanPlayerName;
 
 // Register click on form button with eventlistener
 // https://www.w3schools.com/js/tryit.asp?filename=tryjs_addeventlistener_displaydate
